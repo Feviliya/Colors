a0f736e40c6a93309ffa50035261f008cb145aa1
Feviliya <feviliya28@gmail.com>
2023-02-23 11:42:04 +0530

    first commit

--- src/main/java/com/example/demo/ControllerDemo2.java
+++ src/main/java/com/example/demo/ControllerDemo2.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class ControllerDemo2 {
-	@GetMapping("/hello")
-		public String hello() {
-		return "Welcome to String Boot";
+		@Value("${value}")
+		private String name; 
+		
+		@GetMapping("/hello")
+		public String hell() {
+		return "Welcome to String Boot"+name;
 	}
 }
